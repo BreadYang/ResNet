982685e04dafc0a10fac25203a9309a655e45197
dev <sudevbohra@gmail.com>
2017-04-29 16:47:35 -0400

    training code incorporated with buildCNN

--- resnet50_training.py
+++ resnet50_training.py
@@ -28,19 +28,20 @@ import keras
 from keras import applications
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
-from keras.models import Sequential
+from keras.models import Sequential, Model, load_model
 from keras.layers import Dropout, Flatten, Dense
 import time
+import build_CNN
 
 
 img_width, img_height = 224, 224
 
-train_data_dir = '/media/dev/Maxtor/linux/image_net/ILSVRC2012_img_val'
-validation_data_dir = '/media/dev/Maxtor/linux/image_net/ILSVRC2012_img_val'
-nb_train_samples = 50000
-nb_validation_samples = 50000
+train_data_dir = '/home/dev/Documents/10703-project/tiny-imagenet-200/train'
+validation_data_dir = '/home/dev/Documents/10703-project/tiny-imagenet-200/val'
+nb_train_samples = 100000
+nb_validation_samples = 10000
 epochs = 50
-batch_size = 16
+batch_size = 64
 
 
 def tf_config_allow_growth_gpu():
@@ -49,20 +50,45 @@ def tf_config_allow_growth_gpu():
     sess = tf.Session(config=config)
     K.set_session(sess)
 
+
+def pop_layer(model):
+    if not model.outputs:
+        raise Exception('Sequential model cannot be popped: model is empty.')
+
+    model.layers.pop()
+    if not model.layers:
+        model.outputs = []
+        model.inbound_nodes = []
+        model.outbound_nodes = []
+    else:
+        model.layers[-1].outbound_nodes = []
+        model.outputs = [model.layers[-1].output]
+    model.built = False
+
 def init_compile_model():
     # build the ResNet50 network
-    model = applications.ResNet50(include_top=True, weights='imagenet')
+    initial_model = applications.ResNet50(include_top=True)
+    # state = [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0]
+    state = [1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0]
+    initial_model = build_CNN.update_model(state, 'imagenet')
+    pop_layer(initial_model)
+    last = initial_model.layers[-1].output
+    preds = Dense(200, activation='softmax', name="fc2")(last)
+    model = Model(initial_model.input, preds)
+    # model = load_model('my_model.h5')
+    model.load_weights('my_model1.h5')
     print('Model loaded.')
-
+    model.summary()
+    # pop_layer(model)
     # set the first x layers (up to the last conv block)
     # to non-trainable (weights will not be updated)
-    # for layer in model.layers[:25]:
-    #     layer.trainable = False
+    #for layer in model.layers[:25]:
+        #layer.trainable = False
 
     # compile the model with a SGD/momentum optimizer
     # and a very slow learning rate.
-    model.compile(loss='binary_crossentropy',
-                  optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizers.SGD(lr=1e-3, momentum=0.9),
                   metrics=['accuracy'])
     print('Model compiled.')
     return model
@@ -100,8 +126,9 @@ class PrintRuntime(keras.callbacks.Callback):
         end = time.time()
         runtime = end-self.begin
         self.run_time.append(runtime)
-        print("Run-time on last batch:", runtime, "seconds")
-        print("Avg Run-time on last batch:", sum(self.run_time)/len(self.run_time), "seconds")
+        if len(self.run_time)%100:
+            # print("Run-time on last batch:", runtime, "seconds")
+            print("Avg Run-time on last batch:", sum(self.run_time)/len(self.run_time), "seconds")
 
 # fine-tune the model
 
@@ -111,7 +138,9 @@ train_generator, validation_generator = init_generators()
 runtime_callback = PrintRuntime()
 model.fit_generator(
     train_generator,
-    steps_per_epoch=nb_train_samples/batch_size,
-    epochs=epochs,
+    steps_per_epoch=10000/64,
+    epochs=30,
     validation_data=validation_generator,
-    nb_val_samples=nb_validation_samples, verbose=2, callbacks=[runtime_callback])
+    validation_steps=1000/64, verbose=2)
+
+model.save('my_model2.h5')
